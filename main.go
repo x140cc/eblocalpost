53c913259f8b1b3351867c216530f2cde41c6f04
x140cc <x140cc@users.noreply.github.com>
2019-08-09 19:30:56 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,117 +1,112 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/parnurzeal/gorequest"
+	"bytes"
+	"fmt"
 	"net/http"
-	"net/url"
+	"io/ioutil"
+	"encoding/json"
 	"strings"
 	"time"
-	"fmt"
+	"os"
+	"bufio"
 )
 
-func GetQ() string {
 
-	g, _ := url.Parse("http://127.0.0.1:8018/")
-	gq := g.Query()
-	gq.Set("auth", "ebkhttpsqs2016")
-	gq.Set("name", "local")
-	gq.Set("opt", "get")
-	g.RawQuery = gq.Encode()
-	//	res, err := http.Get(u.String())
-//	fmt.Println(g.String())
-	r := gorequest.New()
-	_, body, errs := r.Get(g.String()).
-		Type("multipart").
-		Retry(5, 10 * time.Second, http.StatusBadRequest, http.StatusInternalServerError).
-		End()
-	if errs != nil{
-		return fmt.Sprint(errs)
+
+var senders map[string]bool = make(map[string]bool)
+func readsenders(){
+	file, err := os.Open("/etc/pmta/senders2")
+	if err != nil {
 	}
-
-	return body
-
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		strp := scanner.Text()
+		senders[strp] = true
+	}
 }
-
 func main() {
 
-	for{
-	//	text := `d,2019-01-23 02:49:15-0500,ekaterina.vaulina@eventbank.com,<EI_EDM:711:14151:b4fb79f5-ef1b-47a9-8f58-be851399051f:JavaMail.ebprod@ebcproweb01>,no-reply@eventbankmail.com,eb-share1,com,,"smtp;250 2.6.0 <EI_EDM:711:14151:b4fb79f5-ef1b-47a9-8f58-be851399051f:JavaMail.ebprod@ebcproweb01> [InternalId=11686606012594, Hostname=BN7PR08MB5522.namprd08.prod.outlook.com] 70483 bytes in 1.009, 68.151 KB/sec Queued mail for delivery",,,,,,,`
-		text := GetQ()
-		//fmt.Println(text)
-
-		if strings.Contains(text,"GET_END"){
-			time.Sleep(6 * time.Second)
-			continue
+		readsenders()
+		file, err := os.Open(os.Args[1])
+		if err != nil {
 		}
-		Consume(text)
-	}
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		i := 0
+		for scanner.Scan() {
+			i++
+			strp := scanner.Text()
+			text := strp
+			if text == "HTTPSQS_GET_END" {
+				println("queue empty")
+				time.Sleep(20 * time.Second)
+			}
+			println(i)
+			if strings.Contains(text, ",") {
+				segmenttext := strings.Split(text, ",")
+				if len(segmenttext) > 4 && senders[segmenttext[4]] == true{
+					var rbody []map[string]interface{}
+					t := make(map[string]interface{})
+					if segmenttext[0] == "d" {
+						t["event"] = "delivered"
+					} else {
 
-}
+						t["event"] = "bounce"
+					}
+
+					t["email"] = segmenttext[5]
+					t["Tracking-ID"] = segmenttext[len(segmenttext)-2]
+					println(segmenttext[4])
+					println(segmenttext[5])
 
 
-func Consume(text string) {
-	if strings.Contains(text, ",") {
-		segmenttext := strings.Split(text, ",")
-		if len(segmenttext) > 4 {
-			var rbody []map[string]interface{}
-			t := make(map[string]interface{})
-			if segmenttext[0] == "d" {
-				t["event"] = "delivered"
-			} else if  strings.Contains(segmenttext[7],"hard") {
+					rbody = append(rbody, t)
+					b, _ := json.Marshal(rbody)
+					jssyr := string(b)
 
-				t["event"] = "dropped"
-			}else {
+					var url string = ""
+					println(segmenttext[len(segmenttext)-2])
+					if strings.Contains(segmenttext[len(segmenttext)-2],"usweb"){
 
-				t["event"] = "bounce"
+						url = "https://api.eventbank.com/v1/sendgrid/callback/edm"
+					}else {
+
+						url = "https://api.eventbank.cn/v1/sendgrid/callback/edm"
+					}
+
+
+					fmt.Println("URL:>", url)
+
+					var jsonStr = []byte(jssyr)
+					req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+					req.Header.Set("X-Custom-Header", "myvalue")
+					req.Header.Set("Content-Type", "application/json")
+
+					client := &http.Client{
+					}
+					resp, err := client.Do(req)
+					if err != nil {
+
+						fmt.Println("post fail")
+					}
+					defer resp.Body.Close()
+
+					fmt.Println("response Status:", resp.Status)
+					fmt.Println("response Headers:", resp.Header)
+					body, _ := ioutil.ReadAll(resp.Body)
+					fmt.Println("response Body:", string(body))
+				}
+
 			}
 
-			t["email"] = segmenttext[2]
-
-			t["Tracking-ID"] = segmenttext[3]
-
-			rbody = append(rbody, t)
-			b, _ := json.Marshal(rbody)
-			jssyr := string(b)
-
-			var burl string
-
-			if strings.Contains(segmenttext[6],"cn"){
-				burl = "https://api.eventbank.cn/v1/sendgrid/callback/edm"
-
-			}else if strings.Contains(segmenttext[6],"com"){
-				burl = "https://api.eventbank.com/v1/sendgrid/callback/edm"
-
-			}else {
-
-				burl = "https://api.eventbank.com/v1/sendgrid/callback/edm"
-			}
-
-			request := gorequest.New()
-			_, body, _ := request.Post(burl).
-				Retry(5, 10 * time.Second, http.StatusBadRequest, http.StatusInternalServerError).
-				Send(jssyr).
-				End()
-
-	//		var jsonStr = []byte(jssyr)
-			fmt.Println(body)
 
 
-
-
-
-
-
-			//----- process - end
 
 
 		}
 
+
 	}
 
-
-
-
-
-
-}
